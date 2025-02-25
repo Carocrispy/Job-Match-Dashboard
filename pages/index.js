3963812ec2ca05e6ad8a69e5cb85cf8e57358e96
Awode Oluwakemi <awodeoluwakemi@gmail.com>
2025-02-25 12:07:10 +0100

    Updated some files

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,18 @@
 import JobList from "../components/JobList";
+import Footer from "../components/Footer";
 import ThemeToggle from "../components/ThemeToggle";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-white p-10">
+    <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-white p-10 flex flex-col justify-between">
       <div className="flex justify-between items-center mb-5">
         <h1 className="text-2xl font-bold">AI-Powered Job Match Dashboard</h1>
         <ThemeToggle />
       </div>
+
       <JobList />
+
+      <Footer />
     </div>
   );
 }
